ff941b8440473527fb98dbb04d7d681a12d7b773
Yong Kim <yhkim4573@gmail.com>
2023-07-19 18:28:14 -0400

    canvas draw brush size brush color

--- main/app/page.js
+++ main/app/page.js
@@ -12,8 +12,14 @@ export default function Home() {
   const [cursors, setCursors] = useState({});
   const [updateMouse, setUpdateMouse] = useState(false);
   const [colorOpen, setColorOpen] = useState(false);
+
+  const [penDown, setPenDown] = useState(false);
+  const [brushSize, setBrushSize] = useState(10);
+  const [brushColor, setBrushColor] = useState("#000000");
+
   const cursor = useRef();
   const otherCursors = useRef({});
+  const canvasRef = useRef();
 
   useEffect(() => {
     socket.connect();
@@ -83,8 +89,8 @@ export default function Home() {
         gsap.to(cursor.current, {
           x: mouseEvent.clientX + 10,
           y: mouseEvent.clientY + 10,
-          duration: 0.5,
-          ease: "back.out(1.7)",
+          duration: 0.1,
+          ease: "back.out(2.2)",
         });
       }
     }
@@ -158,12 +164,39 @@ export default function Home() {
     cursor.current.style.backgroundColor = element.target.value;
   }
 
+  function draw(coord, size, color) {
+    let ctx = canvasRef.current.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(coord.x, coord.y, size, 0, 2 * Math.PI); // x, y, size
+    ctx.fillStyle = color;
+    ctx.fill();
+  }
+
+  function drawStart(event) {
+    let coord = { x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY };
+    setPenDown(true);
+    draw(coord, brushSize, brushColor);
+    console.log("down");
+  }
+
+  function readyDraw(event) {
+    let coord = { x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY };
+    if (penDown) {
+      // draw
+      draw(coord, brushSize, brushColor);
+    }
+  }
+
+  function endDraw(event) {
+    setPenDown(false);
+  }
+
   return (
     loaded && (
       <>
         <span
           id="cursor"
-          className="h-6 w-6 bg-white absolute cursor-none select-none"
+          className="h-6 w-6 bg-white absolute cursor-none select-none border border-black"
           ref={cursor}
         >
           <span className="relative top-6 flex justify-center whitespace-nowrap">
@@ -182,34 +215,60 @@ export default function Home() {
             </span>
           </span>
         ))}
-        <main className="flex min-h-screen flex-row justify-center p-24 flex-wrap cursor-none">
-          <div className="flex gap-x-1 flex-1 justify-center h-fit">
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Enter username"
-              value={username}
-              maxLength={15}
-              className="text-black px-2 outline-none py-1 rounded-l-sm"
+        <main className="min-h-screen cursor-none flex flex-col items-center select-none">
+          <div className="flex flex-row justify-center p-24 flex-wrap w-full">
+            <div className="flex gap-x-1 flex-1 justify-center h-fit">
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter username"
+                value={username}
+                maxLength={15}
+                className="text-black px-2 outline-none py-1 rounded-l-sm"
+              />
+              <button
+                onClick={submitUsername}
+                className="bg-white text-black px-2 py-1 rounded-r-sm"
+              >
+                Confirm
+              </button>
+              <input
+                type="color"
+                onChange={changeCursorColor}
+                onClick={onClickColor}
+                onFocus={(e) => setColorOpen(true)}
+                onBlur={(e) => setColorOpen(false)}
+              />
+            </div>
+            <div className="border-white border p-6 grid col-auto h-fit max-h-64 overflow-y-auto">
+              <h1>current_users : {users.length}</h1>
+              {users.map((user) => (
+                <span key={user.id}>{user.username}</span>
+              ))}
+            </div>
+          </div>
+          <div className="w-full justify-center grid auto-cols-auto">
+            <canvas
+              width="1080px"
+              height="512px"
+              className="bg-white"
+              onMouseDown={drawStart}
+              onMouseMove={readyDraw}
+              onMouseUp={endDraw}
+              ref={canvasRef}
             />
-            <button
-              onClick={submitUsername}
-              className="bg-white text-black px-2 py-1 rounded-r-sm"
-            >
-              Confirm
-            </button>
+            <input
+              type="range"
+              min={1}
+              max={25}
+              step={0.5}
+              value={brushSize}
+              onChange={(e) => setBrushSize(e.target.value)}
+              className="w-32"
+            />
             <input
               type="color"
-              onChange={changeCursorColor}
-              onClick={onClickColor}
-              onFocus={(e) => setColorOpen(true)}
-              onBlur={(e) => setColorOpen(false)}
+              onChange={(e) => setBrushColor(e.target.value)}
             />
-          </div>
-          <div className="border-white border p-6 grid col-auto h-fit max-h-64 overflow-y-auto">
-            <h1>current_users : {users.length}</h1>
-            {users.map((user) => (
-              <span key={user.id}>{user.username}</span>
-            ))}
           </div>
         </main>
       </>
